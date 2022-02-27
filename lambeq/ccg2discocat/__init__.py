834f9ec2b8ec620baec1a079c047755bcc2bca6e
Hadi <67070321+hwazni@users.noreply.github.com>
2022-02-27 23:56:05 +0000

    Update __init__.py

--- lambeq/ccg2discocat/__init__.py
+++ lambeq/ccg2discocat/__init__.py
@@ -1,4 +1,19 @@
-__all__ = ['CCGBankParser', 'DepCCGParser']
+__all__ = ['CCGRule',
+           'CCGRuleUseError',
+           'CCGTree',
 
-from lambeq.ccg2discocat.ccgbank_parser import CCGBankParser
-from lambeq.ccg2discocat.depccg_parser import DepCCGParser
+           'CCGParser',
+           'CCGBankParseError',
+           'CCGBankParser',
+           'DepCCGParseError',
+           'DepCCGParser',
+           'WebParseError',
+           'WebParser']
+
+from lambeq.ccg2discocat.ccg_rule import CCGRule, CCGRuleUseError
+from lambeq.ccg2discocat.ccg_tree import CCGTree
+
+from lambeq.ccg2discocat.ccg_parser import CCGParser
+from lambeq.ccg2discocat.ccgbank_parser import CCGBankParseError, CCGBankParser
+from lambeq.ccg2discocat.depccg_parser import DepCCGParseError, DepCCGParser
+from lambeq.ccg2discocat.web_parser import WebParseError, WebParser
